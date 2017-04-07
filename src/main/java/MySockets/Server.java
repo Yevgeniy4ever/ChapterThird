d78dffaf68136da37e6d0bb22f771f58c192ee07
Developer <ponomaryov.jenya@gmail.com>
2017-04-07 20:39:26 +0700

    Second

--- src/main/java/MySockets/Server.java
+++ src/main/java/MySockets/Server.java
@@ -13,13 +13,25 @@ import java.net.Socket;
  */
 public class Server {
 
-    static final int HOST = 19000;
+    static final int PORT = 19000;
 
     public static void main(String[] args) {
         ServerSocket sSocket = null;
         try {
-            sSocket = new ServerSocket(HOST);
+            sSocket = new ServerSocket(PORT);
+
+            System.out.println("Started, waiting for connection");
+
             Socket socket = sSocket.accept();
+
+            System.out.println("Accepted. " + socket.getInetAddress() + " getPort() "
+                    + socket.getPort()+ " getLocalPort() " + socket.getLocalPort());
+
+            Thread thread = new Thread();
+
+
+            thread.start();
+
             try(InputStream in = socket.getInputStream();
                 OutputStream out = socket.getOutputStream()){
 
@@ -29,11 +41,45 @@ public class Server {
 
                 byte[] data = new byte[32*1024];
                 int readBytes = in.read(data);
+
+                System.out.println("data: " + data + " readBytes: " + data + "\n");
+
+                System.out.println(new String(data, 0, readBytes));
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             IOUtils.closeQuietly(sSocket);
         }
+
+
     }
 }
+
+    class ClientConnected implements Runnable{
+
+        @Override
+        public void run(Socket socket) throws IOException {
+
+            try(InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream()){
+
+                String send = "How are you?";
+                out.write(send.getBytes());
+                out.flush();
+
+                byte[] data = new byte[32*1024];
+                int readBytes = in.read(data);
+
+                System.out.println("data: " + data + " readBytes: " + data + "\n");
+
+                System.out.println(new String(data, 0, readBytes));
+            }
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
