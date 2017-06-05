90125b3c81c05995c8ed275ec1e14f5d181cf406
Yevgeniy <archangel.inside.me@gmail.com>
2017-06-05 08:50:23 +0600

    Shildt 15. Introduction to applets

---
+++ src/main/java/shiltd/appletsandevents/AppletSkel.java
@@ -0,0 +1,39 @@
+package shiltd.appletsandevents;
+import java.applet.*;
+import java.awt.*;
+
+/**
+ * Created by Yevgeniy on 04.06.2017.
+ */
+
+/*
+<applet code="AppletSkel" width = 300; height = 100>
+</applet>
+ */
+public class AppletSkel extends Applet{
+    // the method is called first
+    public void init(){
+        //initialization
+    }
+
+    //the method is called second after method init.
+    //It is also called whenever the applet is restarted
+    public void start(){
+        //starting and resumeption
+    }
+
+    //the method is called for stoping of applet
+    public void stop(){
+
+    }
+
+    public void destroy(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+
+
+}
---
+++ src/main/java/shiltd/appletsandevents/SimpleApplet.java
@@ -0,0 +1,14 @@
+package shiltd.appletsandevents;
+
+import java.applet.*;
+import java.awt.*;
+
+/**
+ * Created by Yevgeniy on 04.06.2017.
+ */
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Java makes applets easy.", 20, 20);
+    }
+
+}
