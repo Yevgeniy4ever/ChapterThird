7f35e41654d62898d5f66a6cf44b5e829ca9c1ae
Developer <ponomaryov.jenya@gmail.com>
2017-06-05 14:33:12 +0600

    Shildt  15. Applets\Events\Assert\Instanceof\Transient\Native\This\Volatile\Strictfp

---
+++ src/main/java/shiltd/appletsandevents/MyClass.java
@@ -0,0 +1,18 @@
+package shiltd.appletsandevents;
+
+/**
+ * Created by Developer on 6/5/2017.
+ */
+public class MyClass {
+    int a;
+    int b;
+
+    MyClass(int i, int j){
+        a = i;
+        b = j;
+    }
+
+    MyClass(int i) {
+        this(i, i);
+    }
+}
